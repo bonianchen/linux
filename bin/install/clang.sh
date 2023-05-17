8ca6e2ec3213d80ca6c99bb3ca7ada4774d69dc4
Bonian Chen <bonian.chen@gmail.com>
2023-05-17 07:21:32 +0000

    .

--- bin/install/clang.sh
+++ bin/install/clang.sh
@@ -3,8 +3,11 @@
 APP_PATH=`which clang`
 
 if [ -z "${APP_PATH}" ]; then
-    sudo apt install -y lsb-release wget software-properties-common gnupg
-    sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+    if [ ! -e /usr/bin/clang-15 ]; then
+        sudo apt install -y lsb-release wget software-properties-common gnupg
+        sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+    fi
+    ln -s /usr/bin/clang-15 $HOME/bin/clang
 fi
 
 export PATH
