b5f7f173bbdb8479f8bffea011d0089e8f8d0e74
Bonian Chen <bonian.ch2@gmail.com>
2023-05-19 18:24:13 +0000

    .

--- bin/install/chrome.sh
+++ bin/install/chrome.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 #curl -L -o google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo dpkg -i google-chrome-stable_current_amd64.deb
-curl -L -o /tmp/google-chrome-beta_amd64.deb https://dl.google.com/linux/direct/google-chrome-beta_amd64.deb
-sudo dpkg -i /tmp/google-chrome-beta_amd64.deb
+sudo apt-get install -y google-chrome-beta
