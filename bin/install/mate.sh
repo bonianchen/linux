026092d41a2adb223aaadfe735c417647fff3a7a
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-05-31 09:22:29 +0800

    Update mate.sh

--- bin/install/mate.sh
+++ bin/install/mate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update -y | cut -d' ' -f1
 sudo apt upgrade -y | cut -d' ' -f1
-sudo apt install -y mate libasound2 libasound2-plugins alsa-utils alsa-oss pulseaudio pulseaudio-utils
+sudo apt install -y mate libasound2 libasound2-plugins alsa-utils alsa-oss pulseaudio pulseaudio-utils pavucontrol
 sudo usermod -aG pulse,pulseaccess,audio $USER
 sudo systemctl set-default graphical.target &> /dev/null 
