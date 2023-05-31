f7b036d198ad9d5cc3add883c045db2bc7eddc49
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-05-31 11:32:48 +0800

    Update chrome.sh

--- bin/install/chrome.sh
+++ bin/install/chrome.sh
@@ -7,6 +7,7 @@ if [ -z "${APP_PATH}" ]; then
     #sudo dpkg -i google-chrome-stable_current_amd64.deb
     curl -L -o google-chrome-beta_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
     sudo dpkg -i google-chrome-beta_current_amd64.deb
+    sudo apt-get -y --fix-broken upgrade
 fi
 
 export PATH
