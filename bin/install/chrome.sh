390ae13afdc8aa92b317f48c8235cd99fcec9e1a
Bonian Chen <bonian.ch@gmail.com>
2023-05-31 04:38:41 +0000

    .

--- bin/install/chrome.sh
+++ bin/install/chrome.sh
@@ -3,11 +3,14 @@
 APP_PATH=`which chrome`
 
 if [ -z "${APP_PATH}" ]; then
+    if [ ! -e /opt/google/chrome-beta ]; then
     #curl -L -o google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     #sudo dpkg -i google-chrome-stable_current_amd64.deb
     curl -L -o /tmp/google-chrome-beta_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
     sudo dpkg -i /tmp/google-chrome-beta_current_amd64.deb
     sudo apt-get -y --fix-broken upgrade
+    fi
+    ln -s /opt/google/chrome-beta/chrome $HOME/bin/chrome
 fi
 
 export PATH
