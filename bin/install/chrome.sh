4415ccc66de5e5f86014f77fb53c05693ad784d6
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-05-31 11:28:23 +0800

    Update chrome.sh

--- bin/install/chrome.sh
+++ bin/install/chrome.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-#curl -L -o google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i google-chrome-stable_current_amd64.deb
-#sudo apt-get install -y google-chrome-beta
-curl -L -o google-chrome-beta_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
-sudo dpkg -i google-chrome-beta_current_amd64.deb
+
+APP_PATH=`which chrome`
+
+if [ -z "${APP_PATH}" ]; then
+    #curl -L -o google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    #sudo dpkg -i google-chrome-stable_current_amd64.deb
+    curl -L -o google-chrome-beta_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-beta_current_amd64.deb
+    sudo dpkg -i google-chrome-beta_current_amd64.deb
+fi
+
+export PATH
+echo "${PATH}"
